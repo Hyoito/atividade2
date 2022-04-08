9862badcadb32fe32ac424d509bfd8b56e67cb7f
Breno Andrade <paodemel099@hotmail.com>
2022-04-08 16:50:22 -0300

    add new file

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+// questão 1
+// function soma(n1: number, n2: number){
+//     let soma = n1 + n2;
+//     console.log("Soma: ", soma)
+// }
+
+// soma(10,20)
+
+// questão 2
+// class Pessoa{
+//     nome: string;
+//     idade: number;
+
+//     constructor(nome: string, idade: number){
+//         this.nome = nome;
+//         this.idade = idade;
+//     }
+
+//     calcular(){
+//         this.idade = this.idade * 12
+//         console.log(`Vc possui ${this.idade} meses`)
+//     }
+// }
+
+// let pessoa = new Pessoa('Breno',20)
+
+// pessoa.calcular()
+
