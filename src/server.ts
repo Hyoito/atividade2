134248e2114499bb513cf8ebb33d51924c6048de
Breno Andrade <paodemel099@hotmail.com>
2022-04-08 17:24:13 -0300

    enter

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 // questão 1
 // function soma(n1: number, n2: number){
-//     let soma = n1 + n2;
-//     console.log("Soma: ", soma)
+//     for( let i = n1; i < n2; i++){   
+//         console.log(`${n1 += (i+1)}`)
+        
+//     }
 // }
+
+// soma(5, 10)
 
 // soma(10,20)
 
-// questão 2
+// // questão 2
 // class Pessoa{
 //     nome: string;
 //     idade: number;
