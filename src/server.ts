4c98142a7962f942d62e9fc16c77ef7da347d1bd
Breno Andrade <paodemel099@hotmail.com>
2022-04-08 23:30:36 -0300

    enter

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 // questão 1
-// function soma(n1: number, n2: number){
-//     for( let i = n1; i < n2; i++){   
-//         console.log(`${n1 += (i+1)}`)
+function soma(n1: number, n2: number){
+    let soma = 0
+    for( let i = n1; i < n2 - 1 ; i++){   
+    
+        console.log(`${soma += (i + 1)}`)
         
-//     }
-// }
+    }
+}
 
-// soma(5, 10)
+soma(5, 10)
 
 // // questão 2
 // class Pessoa{
